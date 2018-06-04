4890e5773306648763e5518b2d58359848d7e466
ëduhdm0105 <duhdm0105@gmail.com>
2018-06-03 22:09:24 -0300

    NEW: Atividade 02 de espresso

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const espresso = require('espresso-logic-minimizer');
-const inputs = require('./inputs').inputs;
+const inputs_atv1 = require('./inputs_atv_01').inputs;
+const inputs_atv2 = require('./inputs_atv_02').inputs;
+
 
 const minimizationParser = (input, list) => {
   let inputVars = input[2]
@@ -13,7 +15,7 @@ const minimizationParser = (input, list) => {
       if (bit === '1') {
         finalExpression += inputVars[index];
       } else if(bit === '0'){
-        finalExpression += `${inputVars[index]}'`;        
+        finalExpression += `${inputVars[index]}'`;
       }
     });
     finalExpression += index !== list.length - 1 ? ' + ' : '';
@@ -22,23 +24,35 @@ const minimizationParser = (input, list) => {
 }
 
 
-Object.keys(inputs.questao_1).forEach(key => {
-  console.log('----------------------------------------------------');  
+console.log('----------------------------------------------------');
+console.log('QUESTAO 01');
+console.log('----------------------------------------------------');
+
+Object.keys(inputs_atv1.questao_1).forEach(key => {
+  console.log('----------------------------------------------------');
   console.log(`item ${key}`);
-  console.log(`resultado gerado pelo espresso: `, espresso.minimize(inputs.questao_1[key]));  
-  console.log(`expressão traduzida: `, minimizationParser(inputs.questao_1[key], espresso.minimize(inputs.questao_1[key])));
-  console.log('----------------------------------------------------');    
+  console.log(`resultado gerado pelo espresso: `, espresso.minimize(inputs_atv1.questao_1[key]));
+  console.log(`expressão traduzida: `, minimizationParser(inputs_atv1.questao_1[key], espresso.minimize(inputs_atv1.questao_1[key])));
+  console.log('----------------------------------------------------');
 });
 
-console.log('----------------------------------------------------');   
-console.log('Questão 02');     
-console.log('resultado espresso = ', espresso.minimize(inputs.questao_2));
-console.log('----------------------------------------------------');    
+console.log('----------------------------------------------------');
+console.log('Questão 02');
+console.log('resultado espresso = ', espresso.minimize(inputs_atv1.questao_2));
+console.log('----------------------------------------------------');
 
-console.log('----------------------------------------------------');    
-console.log('Questão 03');     
-console.log('resultado espresso = ', espresso.minimize(inputs.questao_3));
-console.log('----------------------------------------------------');    
+console.log('----------------------------------------------------');
+console.log('Questão 03');
+console.log('resultado espresso = ', espresso.minimize(inputs_atv1.questao_3));
+console.log('----------------------------------------------------');
+console.log('----------------------------------------------------');
+console.log('QUESTAO 02');
+console.log('----------------------------------------------------');
 
-
-
+Object.keys(inputs_atv2.questao_1.a).forEach(key => {
+  console.log('----------------------------------------------------');
+  console.log(`item ${key}`);
+  console.log(`resultado gerado pelo espresso: `, espresso.minimize(inputs_atv2.questao_1.a[key]));
+  console.log(`expressão traduzida: `, minimizationParser(inputs_atv2.questao_1.a[key], espresso.minimize(inputs_atv2.questao_1.a[key])));
+  console.log('----------------------------------------------------');
+});
